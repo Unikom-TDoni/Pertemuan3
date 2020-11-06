5efd7f61d27130606ea145d34a257755368494ea
lncodes <lastnightcodes@gmail.com>
2020-11-06 22:30:34 +0700

    Squashed 'PBO10K-10119907-Latihan38-Lingkaran/' content from commit 9b28129
    git-subtree-dir: PBO10K-10119907-Latihan38-Lingkaran
    git-subtree-split: 9b2812926f4b3c52d7829efbb2a559628ec4e18a

---
+++ src/main/java/id/tdoni/bangundatar/KalkulatorBangunDatar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.tdoni.bangundatar;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk kalkulasi lingkaran
+ */
+public abstract class KalkulatorBangunDatar {
+    public abstract Number getLuas();
+    public abstract Number getKeliling();
+}
